6dfa2d9e765a8b11322583e2164780112fd7f58a
swatip002 <swatiprakrutidas2002@gmail.com>
2025-06-27 00:10:33 +0530

    added auth Middleware

---
+++ middlewares/auth.middlewares.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+const  User = require('../models/user.model');
+
+module.exports = async (req, res, next) => {
+    const token = req.headers.authorization?.split(' ')[1];
+    if (!token){
+        if (!token) return res.status(401).json({ error: 'Unauthorized' });
+    }
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = await User.findById(decoded.id).select('-password');
+        next();
+    }
+    catch (error){
+        console.error(error);
+        return res.status(401).json({error: 'Unauthorized'});
+    }
+}
